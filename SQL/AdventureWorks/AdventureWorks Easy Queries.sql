0e491c5392e8ba2413fe96045750c35976c8e591
Simon Li <jet.laxmario07@gmail.com>
2021-07-04 12:03:49 -0400

    Updating easy. Adding medium.

--- SQL/AdventureWorks/AdventureWorks Easy Queries.sql
+++ SQL/AdventureWorks/AdventureWorks Easy Queries.sql
@@ -1,15 +1,15 @@
 /*Based on Microsoft's AdventureWorks database. PKs are always listed first. Refer to image for FKs if needed
 
-Customer(CustomerID, FirstName, MiddleName, LastName, CompanyName, EmailAddress)
-CustomerAddress(CustomerID, AddressID, AddressType)
-Address(AddressID, AddressLine1, AddressLine2, City, StateProvince, CountyRegion, PostalCode)
-SalesOrderHeader(SalesOrderID, RevisionNumber, OrderDate, CustomerID, BillToAddressID, ShipToAddressID, ShipMethod, SubTotal, TaxAmt, Freight)
-SalesOrderDetail(SalesOrderID, SalesOrderDetailID, OrderQty, ProductID, UnitPrice, UnitPriceDiscount)
-Product(ProductID, Name, Color, ListPrice, Size, Weight, ProductModelID, ProductCategoryID)
-ProductModel(ProductModelID, Name)
-ProductCategory(ProductCategoryID, ParentProductCategoryID, Name)
-ProductModelProductDescription(ProductModelID, ProductDescriptionID, Culture)
-ProductDescription(ProductDescriptionID, Description)*/
+Customer: CustomerID | FirstName | MiddleName | LastName | CompanyName | EmailAddress
+CustomerAddress: CustomerID | AddressID | AddressType
+Address: AddressID | AddressLine1 | AddressLine2 | City | StateProvince | CountyRegion | PostalCode
+SalesOrderHeader: SalesOrderID | RevisionNumber | OrderDate | CustomerID | BillToAddressID | ShipToAddressID | ShipMethod | SubTotal | TaxAmt | Freight
+SalesOrderDetail: SalesOrderID | SalesOrderDetailID | OrderQty | ProductID | UnitPrice | UnitPriceDiscount
+Product: ProductID | Name | Color | ListPrice | Size | Weight | ProductModelID | ProductCategoryID
+ProductModel: ProductModelID | Name
+ProductCategory: ProductCategoryID | ParentProductCategoryID | Name
+ProductModelProductDescription: ProductModelID | ProductDescriptionID | Culture
+ProductDescription: ProductDescriptionID | Description */
 
 --Easy questions
 --1) Show the first name and the email address of customer with CompanyName 'Bike World'
