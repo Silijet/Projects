dc84b29fee0ddfccf7ab575fde2f2deb455efd02
Simon Li <jet.laxmario07@gmail.com>
2021-07-05 00:57:27 -0400

    First half of med questions

--- SQL/AdventureWorks/AdventureWorks Medium Queries.sql
+++ SQL/AdventureWorks/AdventureWorks Medium Queries.sql
@@ -12,3 +12,35 @@ ProductDescription: ProductDescriptionID | Description */
 
 --Medium Questions
 --1) A "Single Item Order" is a customer order where only one item is ordered. Show the SalesOrderID and the UnitPrice for every Single Item Order.
+--Output: SalesOrderID | UnitPrice
+--Where OrderQty = 1
+
+SELECT sod.salesorderid
+    ,sod.unitprice
+FROM 
+    salesorderdetail sod
+WHERE
+    orderqty = 1;
+
+--2) Where did the racing socks go? List the product name and the CompanyName for all Customers who ordered ProductModel 'Racing Socks'.
+--Output Product.Name | Customer.CompanyName
+--WHERE ProductModel.Name = 'Racing Socks'
+
+SELECT 
+    prod.name
+    ,cust.companyname
+FROM 
+    customer cust
+    INNER JOIN salesorderheader soh
+    ON cust.customerid = soh.customerid
+    INNER JOIN salesorderdetail sod
+    ON soh.salesorderid = sod.salesorderid
+    INNER JOIN product prod
+    ON sod.productid = prod.productid
+    INNER JOIN productmodel prodmod
+    ON prod.productmodelid = prodmod.productmodelid
+WHERE
+    (1=1)
+    AND prodmod.name = 'Racing Socks';
+
+--3)
