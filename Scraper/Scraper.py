5760f66494cc2b1955cbddc831e8f4fa140f5bcf
Simon Li <jet.laxmario07@gmail.com>
2021-07-11 11:43:55 -0400

    Fixed for loop. Added try catch. Tried to make ending year more robust

--- Scraper/Scraper.py
+++ Scraper/Scraper.py
@@ -1,6 +1,6 @@
 #Get beautiful soup
 from bs4 import BeautifulSoup
-Import requests
+from datetime import datetime
 #Function that improves connection to URL
 s=requests.session()
 i = 1920
@@ -8,11 +8,16 @@ i = 1920
 with open('plane_crash_data.csv', mode='w') as f
     # define header here either as an array or a dict
     csv.writerow() # header, which you only want to write once
-    for i <= 2021:
-        # put in try catch0
-        response = s.get(f'www.planecrashinfo.com/{i}/{i}')
+    for i <= datetime.now().year:
+        # put in try catch
+        try:
+            response = s.get(f'www.planecrashinfo.com/{i}/{i}')
         # this should be the html skeleton of the page
-        soup = BeautifulSoup(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
+            i = i+1
+        except:
+            print("Response error")
+            quit()
         # parse based on attribute names
         # and match the format of the header
         # the data struct you store the data in will be the same data struct as the header
