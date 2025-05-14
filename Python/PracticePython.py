cab76625a6dbfea075926417aede9b231ab97a57
Simon Li <34788763+Silijet@users.noreply.github.com>
2025-05-13 20:35:28 -0400

    Update PracticePython.py

--- Python/PracticePython.py
+++ Python/PracticePython.py
@@ -63,7 +63,28 @@ try:
     if input_number % elem == 0:
       y.append(elem)
   
-  print("The factors of {input_number} are: {y}")
+  print(f"The factors of {input_number} are: {y}")
 
 except ValueError:
   print("Please enter a valid integer.")
+
+  
+#Exercise 5
+#  Take two lists and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+d = []
+#Remove duplicates
+for elem in a:
+  if elem not in c:
+    c.append(elem)
+    
+try:
+  for elem in b:
+    if elem in c:
+      d.append(elem)
+  print(f"Common elements: {d}")
+except Exception as e:
+  print("An error occurred:", e)
